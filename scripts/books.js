5ba2a23e871f9427dd26102f0d464ec0bc06a668
Brent Williams <brenton_williams@mac.com>
2017-05-24 15:37:22 -0700

    books1.1

---
+++ scripts/books.js
@@ -0,0 +1,15 @@
+//book 1
+var albumPicasso = {
+     title: 'Getting Things Done',
+     author: 'David Allen',
+     publisher: 'Penguin',
+     year: '2001',
+     albumArtUrl: 'assets/200px-Getting_Things_Done.jpg',
+     chapter: [
+         { title: 'A New Practice for a New Reality', page: '10' },
+         { title: 'Getting Control of Your Life: The Five Stages of Mastering Workflow', duration: '23' },
+         { title: 'Getting Projects Creatively Under Way: The Five Phases of Project Planning', duration: '39' },
+         { title: 'Setting Up the Time, Space and Tools', duration: '45'},
+         { title: 'Collection: Corralling Your "Stuff"', duration: '61'}
+     ]
+ };
